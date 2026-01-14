9d45ecc6fca98ed73492313d62c1e8747467356b
archbaer <pereirakebaili@gmail.com>
2026-01-14 11:22:53 +0000

    variables and print statements

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var taskOne string = "Watch TWN golang full course"
+	var rewardDessert string = "Reward myself with a donut"
+	shortGolang := "Watch golang crash course"
+
+	fmt.Println("##### List of Tasks #####")
+
+	fmt.Println()
+	fmt.Println("Tutorials")
+	fmt.Println(taskOne)
+	fmt.Println(shortGolang)
+
+	println()
+	println("Rewards")
+	println(rewardDessert)
+
+	println()
+	println("My Project")
+	println(taskOne)
+	println(shortGolang)
+
+}
