c8764cc609fe03f26cfbe0e13b1af1571aa035ef
archbaer <pereirakebaili@gmail.com>
2026-01-15 11:50:32 +0000

    http endpoints

--- main.go
+++ main.go
@@ -1,26 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var taskItems = []string{
+	"Learn Go",
+	"Build a web app",
+	"Deploy the app",
+}
 
 func main() {
-	var taskOne string = "Watch TWN golang full course"
-	var rewardDessert string = "Reward myself with a donut"
-	shortGolang := "Watch golang crash course"
+	fmt.Println("##### Welcome to my ToDo App")
 
-	fmt.Println("##### List of Tasks #####")
+	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/show-tasks", showTasks)
 
-	fmt.Println()
-	fmt.Println("Tutorials")
-	fmt.Println(taskOne)
-	fmt.Println(shortGolang)
-
-	println()
-	println("Rewards")
-	println(rewardDessert)
-
-	println()
-	println("My Project")
-	println(taskOne)
-	println(shortGolang)
-
+	http.ListenAndServe(":8080", nil)
 }
+
+func showTasks(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("List of tasks:")
+	for i, item := range taskItems {
+		fmt.Fprintf(w, "%d. %s\n", i+1, item)
+	}
+}
+
+func printTasks(arr []string) {
+	for i, item := range arr {
+		fmt.Printf("%d. %s\n", i+1, item)
+	}
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+// func addTasks()
